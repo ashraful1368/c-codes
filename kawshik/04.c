fa6e9f7c2cc2e2dabba9a9d68df68ee3bc7a3a69
Md Ashraful Islam <ashrafulislam7287@gmail.com>
2026-06-30 11:53:47 +0600

    Update 04.c

--- kawshik/04.c
+++ kawshik/04.c
@@ -1,6 +1,30 @@
 #include<stdio.h>
 int main ()
 {
-    
+    int a,b;
+    printf ("enter two number a and b: ");
+    scanf ("%d %d", &a,&b);
+    if (a>b)
+    {
+        printf ("a>b\n");
+    }
+    else{
+        printf ("a<b\n");
+    }
+    if (a>=b)
+    {
+        printf ("a>=b\n");
+    }
+    else{
+        printf ("a<=b\n");
+    }
+    if (a==b)
+    {
+        printf ("a==b\n");
+    }
+    else{
+        printf ("a!=b");
+    }
+
     return 0;
 }
